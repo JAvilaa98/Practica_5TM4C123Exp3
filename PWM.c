b727d233d61083235afd5d367e8f234eff8cc7c0
JAvilaa98 <113447760+JAvilaa98@users.noreply.github.com>
2023-01-29 20:09:02 -0600

    Actualización
    Actualicé los puertos a utilizar para el PWM0 ya que los que tenía anteriormente no me encendían el LED de ninguna forma,  ya con estos nuevos puertos el led enciende correctamente

--- PWM.c
+++ PWM.c
@@ -1,37 +1,35 @@
 
 #include "lib/include.h"
 
-/* Tomando el Módulo0PWM0 Generador 0 -> PB6
-   Tomando el Módulo0PWM3 Generador 1 -> PB5
-   Tomando el Módulo0PWM5 Generador 2 -> PE5*/
+/* Tomando el Módulo 0 del PWM1 Generador 0 -> PB7
+   Tomando el Módulo0  del PWM2 Generador 1 -> PB4
+   Tomando el Módulo0P del PWM4 Generador 2 -> PE4*/
 
 extern void Configura_Reg_PWM1(uint16_t freq)
 {
-    SYSCTL->RCGCPWM |= (1<<0); // Enable reloj de modulo PWM1 pag 354 Habilitamos PWM0 correspondiente al experimento 1
-    SYSCTL->RCGCGPIO |= (1<<1) | (1<<4); //Habilitamos el GPIOB según lo requerido en el experimento 1 pág 340 y 1233 y también encendemos GPIOD 
-    SYSCTL->RCC &= ~(1<<20);  // Deshabilitamo el divisor según lo requerido en el experimento 1 
+    SYSCTL->RCGCPWM |= (1<<0); // Enable reloj de modulo PWM1 pag 354 Habilitamos PWM0 correspondiente al experimento 3
+    SYSCTL->RCGCGPIO |= (1<<1) | (1<<4); //Habilitamos el GPIOB Y GPIO E según lo requerido para el experimento 3
+    SYSCTL->RCC &= ~(1<<20);  // Deshabilitamo el divisor según lo requerido en el experimento 3 
 
-    GPIOB->AFSEL |= (1<<5); // Habilita función alternativa 672 IMMPORTANTE RECORDAR QUE ESTE ESTA CONTROLADO POR EL PW3
-    GPIOB->AFSEL |= (1<<6); //Recordar que este está controlado689 por el PWM1
-    GPIOE->AFSEL|= (1<<5); //Recordar que este está controlado por el PWM5
+    GPIOB->AFSEL |= (1<<4); // Habilita función alternativa 672 IMMPORTANTE RECORDAR QUE ESTE ESTA CONTROLADO POR EL PWM2
+    GPIOB->AFSEL |= (1<<7); //Recordar que este está controlado por el PWM1
+    GPIOE->AFSEL|= (1<<4); //Recordar que este está controlado por el PWM4
 
-    GPIOB->DIR = (1<<5); // Configura i/o Pag 663 IMMPORTANTE RECORDAR QUE ESTE ESTA CONTROLADO POR EL PW3
-    GPIOB->DIR = (1<<6); //Recordar que este está controlado por el PWM1
-    GPIOE->DIR = (1<<5); //Recordar que este está controlado por el PWM5
+    GPIOB->DIR = (1<<4); // Configura i/o Pag 663 IMMPORTANTE RECORDAR QUE ESTE ESTA CONTROLADO POR EL PW2
+    GPIOB->DIR = (1<<7); //Recordar que este está controlado por el PWM1
+    GPIOE->DIR = (1<<4); //Recordar que este está controlado por el PWM4
 
-    GPIOB->PCTL |= (GPIOB->PCTL&0xF00FFFFF) | 0x04400000; /*Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689*/
-    // GPIOB->PCTL |= (GPIOB->PCTL&0x0FFFFFFF) | 0x40000000; Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689
-    GPIOE->PCTL |= (GPIOE->PCTL&0xFF0FFFFF) | 0x00400000; /*Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689*/
+    GPIOB->PCTL |= (GPIOB->PCTL&0x0FF0FFFF) | 0x40040000; //Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689
+    GPIOE->PCTL |= (GPIOE->PCTL&0xFFF0FFFF) | 0x00040000; //Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689
 
-    GPIOB->DEN |= (1<<6)|(1<<5); // para decirle si es digital o no Pag 682 IMMPORTANTE RECORDAR QUE ESTE ESTA CONTROLADO POR EL PW3
-    // GPIOB->DEN |= (1<<7); Recordar que este está controlado por el PWM1
-    GPIOE->DEN |= (1<<5); //Recordar que este está controlado por el PWM5
+    GPIOB->DEN |= (1<<7)|(1<<4); //Para decirle si es digital o no Pag 682
+    GPIOE->DEN |= (1<<4); //Para decir si es digital o no Pag 682
 
     PWM0->_0_CTL = (0<<0); // PWM0 gen 0
     PWM0->_1_CTL = (0<<0); // PWM0 gen 1
     PWM0->_2_CTL = (0<<0); // PWM0 gen 2
 
-    PWM0->_0_GENA = 0X0000008C; //Configuración en pág 1239
+    PWM0->_0_GENB = 0X0000080C; //Configuración en pág 1239
     PWM0->_1_GENA = 0x0000008C; // Establecemos el modo de trabajo de cuenta regresiva pág 1285 y 1239
     PWM0->_2_GENA = 0X0000008C;
 
@@ -48,7 +46,5 @@ extern void Configura_Reg_PWM1(uint16_t freq)
     PWM0->_2_CTL = (1<<0);
 
 
-    PWM0->ENABLE = (1<<5)|(1<<3)|(1<<0); //Encedemos el PWW1 para trabajar con generador 0
-   // PWM0->ENABLE = (1<<3); Encedemos el PWM2 para trabajar con generador 1 según la tabla de la pág 1233 
-    // PWM0->ENABLE = (1<<5); Encedemos el PW4 para trbajar con el generador 2
+    PWM0->ENABLE = (1<<4)|(1<<2)|(1<<1); //Encedemos el PWW1, PWM2 Y PWM4 para trabajar con generador 0
 }
